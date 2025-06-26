5d1878160f2d51a41cc42f178ac3c43dc6904465
grandpares <aboryan24@gmail.com>
2025-06-25 21:12:02 -0500

    fix: do not replace ublue akmods public key

--- postbuild.sh
+++ postbuild.sh
@@ -8,4 +8,4 @@ mkdir -p /usr/lib/modules/${KERNEL}/extra/it87-extras
 cp /tmp/it87-extras.ko.xz /usr/lib/modules/${KERNEL}/extra/it87-extras/it87-extras.ko.xz
 depmod -a ${KERNEL}
 
-install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/public_key.der
+install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/akmods_grandpares.der
